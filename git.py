0386d041c104553ed57491af25f46b68df688ff7
LUIS JAVIER <javierluis@live.com.ar>
2017-10-11 23:06:01 -0500

    Subo las instrucciones

---
+++ git.py
@@ -0,0 +1,16 @@
+
+
+instrucciones = """
+	Inicializar repositorio: 
+	git init
+	
+	Añadir archivos:
+	git add .
+	
+	Crear commits:
+	git commit -m "el mensaje que se va a poner de commit"
+	
+	Subir cambios:
+	git push
+	"""
+print (instrucciones)
